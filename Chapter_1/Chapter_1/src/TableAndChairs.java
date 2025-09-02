463efdd24914fe4b5870f15c575a25decf71cbc0
JaredKishCodes <missionajk@gmail.com>
2025-09-02 18:22:50 +0800

    created tables and chairs using java

---
+++ Chapter_1/Chapter_1/src/TableAndChairs.java
@@ -0,0 +1,17 @@
+public class TableAndChairs {
+    public static void main(String[] args){
+        System.out.println("x                                   x");
+        System.out.println("x                                   x");
+
+
+        // Row 2
+        System.out.println("x        x x x x x x x x x          x");
+
+        // Row 3
+        System.out.println("xxxxx    x               x      xxxxx");
+
+        // Row 4
+        System.out.println("x   x    x               x      x   x");
+        System.out.println("x   x    x               x      x   x");
+    }
+}
