88836e39e39759e9d766c2c4226121f22198e059
JaredKishCodes <missionajk@gmail.com>
2025-09-02 18:09:12 +0800

    created 3 types of comments in java

---
+++ Chapter_1/Chapter_1/src/Comments.java
@@ -0,0 +1,19 @@
+public class Comments {
+    public static void main(String[] args) {
+        // This is a single-line comment(first comment)
+
+
+        /*This
+        is
+        a
+        multi-line comment(second comment)
+        */
+
+
+        /** This is a Documentation (Javadoc) comment
+         * @author Jared
+         * (third comment)
+         * */
+
+    }
+}
