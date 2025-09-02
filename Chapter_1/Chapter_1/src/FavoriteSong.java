aeb039583b5c70e34a7e742b1e3746d44792f5c1
JaredKishCodes <missionajk@gmail.com>
2025-09-02 17:52:02 +0800

    created FavoriteSong

---
+++ Chapter_1/Chapter_1/src/FavoriteSong.java
@@ -0,0 +1,7 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println(" There are places I remember \n All my life, " +
+                "though some have changed \n some forever, not for " +
+                "better\n some have gone and some remain");
+    }
+}
