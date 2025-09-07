6c1991b8f0d8d19abc4c1ec8761a89363a8ee2e5
JaredKishCodes <missionajk@gmail.com>
2025-09-07 19:49:06 +0800

    FixDebugOne

---
+++ Chapter_1/Chapter_1/src/FixDebugOne.java
@@ -0,0 +1,19 @@
+
+
+public class FixDebug One
+{
+    /* This program displays a greeting */
+    public static void Main(String args)
+    {
+        Systm.out.println("Hello World!")
+    }
+/*
+List down the spotted bugs below:
+e.g.
+- // wrong syntax for access modifier "publlic" instead of "public"
+Bugs:
+1. wrong syntax for class name it should be FixDebugOne
+2. wrong syntax for public static void Main(String args) it should be public static void main(String[] args)
+3. wrong syntax for Systm it should be System.out.println("Hello World!");
+*/
+
