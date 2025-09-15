a3c13774edc8bd2841b2c62f8a080851b1557ae7
JaredKishMissiona-UCM <missionajk2@gmail.com>
2025-09-15 16:06:56 +0800

    Create CompanyInfo.java

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,20 @@
+public class CompanyInfo
+{  
+      public static void main(String[] args)
+      {  
+           displayCompanyName();
+           displayHours();    
+      }
+  
+      public static void displayCompanyName(){
+         System.out.println("Smart Electronics\n");
+      }
+  
+      public static void displayHours(){         
+         System.out.println("Monday - Friday 8 am to 6 pm\n");
+         System.out.println("Saturday        8 am to noon\n");
+         System.out.println("Sunday          closed\n");
+      
+      }
+}
+
